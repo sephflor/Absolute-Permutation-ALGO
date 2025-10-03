f030778cdb1365ad1764da14f07b4d966edfd9af
Sephanne Flor <sephanneflor9@gmail.com>
2025-10-02 23:48:32 -0700

    Create Absolute Permutation.cs

---
+++ Absolute Permutation.cs
@@ -0,0 +1,81 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'absolutePermutation' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER k
+     */
+
+    public static List<int> absolutePermutation(int n, int k)
+    {
+        List<int> result = new List<int>();
+        
+        if (k == 0)
+        {
+            for (int i = 1; i <= n; i++)
+                result.Add(i);
+            return result;
+        }
+        
+        if (n % (2 * k) != 0)
+            return new List<int>() { -1 };
+        
+        bool add = true;
+        for (int i = 1; i <= n; i++)
+        {
+            if (add)
+                result.Add(i + k);
+            else
+                result.Add(i - k);
+            
+            if (i % k == 0)
+                add = !add;
+        }
+        
+        return result;
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int t = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int tItr = 0; tItr < t; tItr++)
+        {
+            string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+            int n = Convert.ToInt32(firstMultipleInput[0]);
+
+            int k = Convert.ToInt32(firstMultipleInput[1]);
+
+            List<int> result = Result.absolutePermutation(n, k);
+
+            textWriter.WriteLine(String.Join(" ", result));
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
